da6dc12e585d5edeb32456cb005c9ed80508c1b7
GuillaumeMeheut <guillaume.meheutpro@gmail.com>
2021-05-24 16:06:37 +0200

    fix

--- src/pages/[category]/[id].tsx
+++ src/pages/[category]/[id].tsx
@@ -1,12 +1,12 @@
 import React, { useState } from "react";
 // import styles from "../styles/Id.module.css";
 
-export default function Id({ data }) {
+export default function Index({ data }: any) {
   console.log(data);
   return <div>{JSON.stringify(data)}</div>;
 }
 
-export const getServerSideProps = async ({ params }) => {
+export const getServerSideProps = async ({ params }: any) => {
   const res = await fetch(
     `https://swapi.dev/api/${params.category}/?search=${params.id}`
   );
